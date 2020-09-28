8c59e97681a2049a8922c4d1fad6c622312e0669
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-09-28 17:25:59 -0500

    finished exercises

---
+++ src/HelloWorld.java
@@ -0,0 +1,56 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name = "codeup";
+        System.out.printf("Hello there, %s. I'm Michael.\n", name);
+
+
+        System.out.print("Hello, ");
+
+
+        System.out.println("Codeup");
+
+        int myFavoriteNumber = 390;
+        System.out.print(myFavoriteNumber);
+
+        String myString = (" is an interesting Number");
+        System.out.println(myString);
+
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        int class = 5;
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y += x;
+//
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(y);
+        System.out.println(x);
+
+     long z  = Integer.MAX_VALUE;
+
+
+        System.out.println(z);
+        System.out.println(Integer.MAX_VALUE);
+
+
+
+
+
+    }
+}
+
