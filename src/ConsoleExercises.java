9a43518fed2d6324072f55b1fb098cce383d34b2
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-10-01 14:36:04 -0500

    Finished exercise1

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ConsoleExercises {
@@ -24,19 +25,68 @@ public class ConsoleExercises {
 //        String sentence = value.nextLine();
 //        System.out.printf("%s\n",sentence);
 
+//
+//    Scanner measure = new Scanner(System.in);
+//    System.out.println("What is the length of the room where you are in in feet?");
+//    String length = measure.nextLine();
+//    int l = Integer.parseInt(length);
+//    System.out.println("What is the width of the room where you are in in feet?");
+//    String width = measure.nextLine();
+//    int w = Integer.parseInt(width);
+//    int p = (2*l) +(2* w);
+//
 
-    Scanner measure = new Scanner(System.in);
-    System.out.println("What is the length of the room where you are in in feet?");
-    String length = measure.nextLine();
-    int l = Integer.parseInt(length);
-    System.out.println("What is the width of the room where you are in in feet?");
-    String width = measure.nextLine();
-    int w = Integer.parseInt(width);
-    int p = (2*l) +(2* w);
+//        int i = 0;
+//        while (i < 10) {
+//            System.out.println("i is " + i);
+//            i++;
+//
+//
+//        }
 
-    System.out.println("Your area is: " + l*w + " feet");
-    System.out.println("Your perimeter is: "+ p +" feet");
+        for(int i = 0; i < 10; i += 1) {
+            System.out.println("i is " + i);
+        }
 
+
+        for(int i = 1; i <= 10; i++) { // <--+
+            if(i % 2 == 1) { //              |
+                continue; // ----------------+
+            }
+            System.out.println(i);
+        }
+
+//*****While Loop*************
+        int i = 0;
+        while (i <= 10) {
+            System.out.println("While loop: i:" + i);
+            i++;
+        }
+
+
+// do While*************use on yes or no *
+        int counter = 0;
+
+         do {
+            System.out.println("Loop counter" + counter);
+            counter++;
+        } while (counter <= 0);
+//∞∞∞∞∞For Loop ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
+        for(int x = 0; x <= 10; x++){
+            System.out.println("val of x is: " + x);
+        }
+//        Break and continue
+        for(int z = 0; z <= 10; z++) {
+            if(z % 2 ==1) {
+                continue; //--------------
+            } else if(z > 5) {
+                break;
+
+            }
+            System.out.println("a = "+ z);
+
+        }
+        System.out.println("finished");
 
     }
 }
--- src/ControlFLowExercises.java
+++ src/ControlFLowExercises.java
@@ -1,2 +1,83 @@
-package PACKAGE_NAME;public class ControlFLowExercises {
+import java.lang.Math;
+public class ControlFLowExercises {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+
+//Create a do-while loop that starts at 2, and displays the number squared
+// on each line while the number
+// is less than 1,000,000
+
+//        long dw3 = 2;
+//        do{
+//            System.out.println(dw3);
+//            dw3 *= dw3;
+//        } while(dw3 <= 1000000);
+
+
+
+//
+//        Alter your loop to count backwards by 5's from 100 to -10.
+//
+//        int dw2 = 105;
+//        do{ dw2 = dw2 - 5;
+//            System.out.println( dw2 );
+//        } while(dw2 > -10);
+
+// Reactor with a for loop ••••••••••••••••••••
+
+
+//        for(int i = 0; i <= 100; i+=2 ){
+//            System.out.println(i);
+//        };
+//
+
+
+
+//Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+
+//        int dw = 0;
+//        do{
+//        System.out.println( dw );
+//            dw = dw + 2
+//
+//        } while(dw <= 100);
+
+
+        // Reactor with a for loop ••••••••••••••••••••
+
+
+//        for(int i = 100; i >= -10; i-=5){
+//            System.out.println(i);
+//        }
+//
+
+
+//While
+//
+//Create an integer variable i with a value of 5.
+//Create a while loop that runs so long as i is less than or equal to 15
+//Each loop iteration, output the current value of i, then increment i by one.
+//Your output should look like this:
+//
+//5 6 7 8 9 10 11 12 13 14 15
+
+
+
+//        int i = 5;
+//        while (i <= 15){
+//            System.out.println(i);
+//            i++;
+//        }
+
+    }
 }
