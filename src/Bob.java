950140a36fef8a147d8312e2375281b54dd065b1
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-10-02 12:04:26 -0500

    finished String exercises Java

--- src/Bob.java
+++ src/Bob.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Bob {
+import com.sun.source.tree.WhileLoopTree;
+
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner bob = new Scanner(System.in);
+        Boolean userCont = true;
+        String message = "Say something to Bob!";
+
+
+
+        do {
+            System.out.println(message);
+            String userResponse = bob.next();
+            int qMark = userResponse.lastIndexOf("?");
+
+            if (qMark > 0) {
+                System.out.println("Sure.");
+            } else if (userResponse.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+
+            } else if (userResponse.isBlank()) {
+                System.out.println("Fine. Be that Way!");
+            } else {
+                System.out.println("Whatever.");
+            };
+            System.out.println(message + "? Y/N");
+            String userYN = bob.next();
+            if (!userYN.equalsIgnoreCase("y")){
+            userCont = false; }
+
+        } while (userCont);
+
+
+    }
 }
