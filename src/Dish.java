13cff55a08a9a0c18de5d8853613de1d5ad28f25
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-10-05 15:30:32 -0500

    mini exercisesfinished

--- src/Dish.java
+++ src/Dish.java
@@ -2,7 +2,7 @@ public class Dish {
     public int costInCents;
     public String nameOfDish;
     public Boolean wouldRecommend;
-
+//instance method***********
     public void printSummary(){
         System.out.printf(
                 "Cost:%d%nName:%s%nRecommended:%s",
@@ -10,6 +10,7 @@ public class Dish {
                 nameOfDish,
                 wouldRecommend);
     }
+
 
 
 
