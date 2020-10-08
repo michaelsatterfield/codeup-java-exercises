389f05bed74d49c388aea945d90cf001e899d8bd
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-10-07 21:13:37 -0500

    exercises for java arrays

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,6 +1,20 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class ServerNameGenerator {
 
 
+        String[] nouns = {"car", "rock", "chair", "coupon", "border", "Montana", "accordion", "pen", "microphone", "book"};
+        final String[] adjectives = {"yellow", "bold", "soft", "organized", "loud", "even", "wacky", "cold", "tart", "rapid"};
+        Random random = new Random();
+        int index = random.nextInt(adjectives.length);
+        System.out.println(adjectives[index]);
 
 
+    }
+    public static void main(String[] args) {
+        System.out.println();
 }
+
+
+
