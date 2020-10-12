608bd608799a7dad637d3170d0ae2d4c74e037ca
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-10-11 21:21:01 -0500

    Added Weapons class and getters and setters and constuctors

--- src/ConsoleAdventureGame/WeaponsArray.java
+++ src/ConsoleAdventureGame/WeaponsArray.java
@@ -3,9 +3,9 @@ package ConsoleAdventureGame;
 public class WeaponsArray {
     public static Weapons[] findAll() {
         return new Weapons[]{
-             new Weapons("Spear(tepoztopilli)","5 ft","16 pts","A weapon with a long shaft and a pointed tip, typically of obsidian, used for thrusting or throwing."),
-             new Weapons("Knife(tecpatl)","2 ft","19 pts","a tecpatl was a flint or obsidian knife with a lanceolate figure and double-edged blade, with elongated ends."),
-             new Weapons("Macuahuitl","4 ft","18 pts","a wooden club with several embedded obsidian blades. The name is derived from the Nahuatl language and means \"hand-wood\".  Its sides are embedded with prismatic blades traditionally made from obsidian."),
+             new Weapons("1: Spear(tepoztopilli)","5 ft","16 pts","A weapon with a long shaft and a pointed tip, typically of obsidian, used for thrusting or throwing."),
+             new Weapons("2: Knife(tecpatl)","2 ft","19 pts","a tecpatl was a flint or obsidian knife with a lanceolate figure and double-edged blade, with elongated ends."),
+             new Weapons("3: Macuahuitl","4 ft","18 pts","a wooden club with several embedded obsidian blades. The name is derived from the Nahuatl language and means \"hand-wood\".  Its sides are embedded with prismatic blades traditionally made from obsidian."),
         };
 
 
